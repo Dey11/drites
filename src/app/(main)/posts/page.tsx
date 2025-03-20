9824618b5612faf3f461d74190aca499d74f0099
Dey11 <shreyandey11@gmail.com>
2025-03-20 09:02:06 +0530

    frontend for posts page

--- src/app/(main)/posts/page.tsx
+++ src/app/(main)/posts/page.tsx
@@ -1,9 +1,24 @@
+import { Bookmark, Heart } from "lucide-react";
+
 import { H2 } from "@/components/typography/h2";
 
 const page = () => {
   return (
-    <div className="mx-auto max-w-screen-lg">
-      <H2 className="pt-5">Latest Posts</H2>
+    <div className="mx-auto max-w-screen-lg px-3">
+      <H2 className="py-5">Latest Posts</H2>
+
+      {post.map(({ id, title, description, likes, author, createdAt }) => (
+        <div key={id} className="mb-2">
+          <PostCard
+            id={id}
+            title={title}
+            description={description}
+            likes={likes}
+            author={author}
+            createdAt={createdAt}
+          />
+        </div>
+      ))}
     </div>
   );
 };
@@ -18,6 +33,38 @@ const post = [
     likes: 100,
     author: "Author 1",
     createdAt: "2023-10-01",
+  },
+  {
+    id: 2,
+    title: "Post 2",
+    description: "This is the description for post 2",
+    likes: 200,
+    author: "Author 2",
+    createdAt: "2023-10-02",
+  },
+  {
+    id: 3,
+    title: "Post 3",
+    description: "This is the description for post 3",
+    likes: 150,
+    author: "Author 3",
+    createdAt: "2023-10-03",
+  },
+  {
+    id: 4,
+    title: "Post 4",
+    description: "This is the description for post 4",
+    likes: 250,
+    author: "Author 4",
+    createdAt: "2023-10-04",
+  },
+  {
+    id: 5,
+    title: "Post 5",
+    description: "This is the description for post 5",
+    likes: 300,
+    author: "Author 5",
+    createdAt: "2023-10-05",
   },
 ];
 
@@ -38,5 +85,30 @@ function PostCard({
   author,
   createdAt,
 }: PostType) {
-  return <div></div>;
+  return (
+    <div className="flex w-full flex-col gap-2 rounded-lg p-5 shadow-md">
+      <div className="flex items-center gap-2">
+        <div className="flex size-8 items-center justify-center rounded-full bg-slate-300">
+          {author.charAt(0).toUpperCase()}
+        </div>
+        <div className="text-sm">
+          <p className="font-medium">{author}</p>
+          <p className="text-xs">{createdAt}</p>
+        </div>
+      </div>
+
+      <h3 className="text-lg font-bold">{title}</h3>
+
+      <p>{description}</p>
+
+      <div className="flex items-center justify-between text-sm">
+        <p className="flex items-center gap-1">
+          <Heart className="size-4 cursor-pointer" />
+          {likes}
+        </p>
+
+        <Bookmark className="size-4 cursor-pointer" />
+      </div>
+    </div>
+  );
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link";
+import { Suspense } from "react";
 
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 import { H3 } from "./typography/h3";
 import { Button } from "./ui/button";
-import { Suspense } from "react";
+import { Input } from "./ui/input";
 
 const Header = () => {
   return (
@@ -13,18 +14,29 @@ const Header = () => {
         <Link href="/">
           <H3>Drites.</H3>
         </Link>
-        <Suspense fallback={<div className="h-8 w-8 animate-pulse rounded-full bg-gray-200" />}>
-        <SignedOut>
-          <Button asChild>
-            <SignInButton
-              forceRedirectUrl={"/posts"}
-              signUpForceRedirectUrl={"/posts"}
-            />
-          </Button>
-        </SignedOut>
-        <SignedIn>
-          <UserButton />
-        </SignedIn>
+        <Suspense
+          fallback={
+            <div className="size-8 animate-pulse rounded-full bg-gray-200" />
+          }
+        >
+          <SignedOut>
+            <Button asChild>
+              <SignInButton
+                forceRedirectUrl={"/posts"}
+                signUpForceRedirectUrl={"/posts"}
+              />
+            </Button>
+          </SignedOut>
+          <SignedIn>
+            <div className="flex items-center gap-x-2">
+              <Input
+                placeholder="Search Posts..."
+                className="mx-2 h-9 border border-black bg-brand-section"
+              />
+              <Button>Create Post</Button>
+              <UserButton />
+            </div>
+          </SignedIn>
         </Suspense>
       </div>
     </header>
