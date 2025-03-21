1e8b2e3d3c9fa7c3e43e05a2bc38f6c0f934c4b4
Dey11 <shreyandey11@gmail.com>
2025-03-21 11:27:46 +0530

    create post action

---
+++ src/app/actions/create-post.ts
@@ -0,0 +1,40 @@
+// "use server";
+import { auth } from "@clerk/nextjs/server";
+
+import { prisma } from "@/lib/prisma";
+
+type PreviousStateType = {
+  success: boolean;
+  message: string;
+};
+
+export async function createPost(
+  previousState: PreviousStateType | null,
+  formData: FormData
+) {
+  try {
+    const session = await auth();
+    if (!session.userId) {
+      throw new Error("User not authenticated");
+    }
+
+    const newPost = await prisma.post.create({
+      data: {
+        title: formData.get("title") as string,
+        content: formData.get("content") as string,
+        authorId: session.userId,
+      },
+    });
+
+    return {
+      success: true,
+      message: "Post created successfully",
+    };
+  } catch (err) {
+    console.error(err);
+    return {
+      success: false,
+      message: "Error creating post",
+    };
+  }
+}
--- src/components/create/post-form.tsx
+++ src/components/create/post-form.tsx
@@ -1,26 +1,19 @@
 "use client";
 
 import Link from "next/link";
-import { useState, useTransition } from "react";
+import { useActionState } from "react";
+
+import { createPost } from "@/app/actions/create-post";
 
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
 import { Textarea } from "../ui/textarea";
 
 export default function CreateForm() {
-  const [error, setError] = useState<string | null>(null);
-  const [loading, setLoading] = useState(false);
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
-  const [isPending, startTransition] = useTransition();
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    startTransition(() => {});
-  };
+  const [state, formAction, isPending] = useActionState(createPost, null);
 
   return (
-    <form onSubmit={handleSubmit} className="flex flex-col gap-y-5">
+    <form action={formAction} className="flex flex-col gap-y-5">
       <div className="flex flex-col gap-y-2">
         <label htmlFor="title" className="">
           Title
@@ -28,10 +21,9 @@ export default function CreateForm() {
         <Input
           id="title"
           type="text"
-          value={title}
+          name="title"
           placeholder="Title"
           className="bg-brand-section"
-          onChange={(e) => setTitle(e.target.value)}
         />
       </div>
 
@@ -41,26 +33,32 @@ export default function CreateForm() {
           id="content"
           placeholder="Type your content in markdown here..."
           className="h-[50dvh] bg-brand-section"
-          value={content}
-          onChange={(e) => setContent(e.target.value)}
+          name="content"
         />
       </div>
 
-      {error && (
-        <div
-          className="rounded-md bg-red-100 p-4 text-center text-red-700"
-          role="alert"
-        >
-          <p>{error}</p>
-        </div>
-      )}
-
       <div className="flex w-full items-center justify-end gap-x-2">
         <Link href="/posts">
-          <Button className="border border-brand-blue bg-white">Cancel</Button>
+          <Button
+            disabled={isPending}
+            className="border border-brand-blue bg-white"
+          >
+            Cancel
+          </Button>
         </Link>
-        <Button type="submit">Publish</Button>
+        <Button disabled={isPending}>Publish</Button>
       </div>
+
+      {state?.success && (
+        <div className="mt-5 flex w-full items-center justify-center rounded-lg bg-green-100 p-4 text-sm text-green-700">
+          {state.message}
+        </div>
+      )}
+      {state?.success === false && (
+        <div className="mt-5 flex w-full items-center justify-center rounded-lg bg-red-100 p-4 text-sm text-red-700">
+          {state.message}
+        </div>
+      )}
     </form>
   );
 }
